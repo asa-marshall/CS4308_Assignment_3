f74c2fa8260ecbdd64a372fd316b10f232670fe6
asa <asam268@gmail.com>
2020-03-09 13:19:32 -0400

    Prompts user for student name, student id, and number of courses. Asks user if they wish to continue adding student information. Adds all student information to an array and prints.

--- __main__.py
+++ __main__.py
@@ -1,8 +1,39 @@
 from Student import Student
 
+
+def continue_adding():
+    """Prompts user on whether to continue adding students
+
+    :return: bool
+    """
+    while True:
+        print("Input another student? (y/n): ")
+        contin = input()
+        if not contin == 'n' and not contin == 'y':
+            print("Invalid input. Use 'y' or 'n'.")
+        elif contin == "y":
+            return True
+        else:
+            return False
+
+
 if __name__ == '__main__':
-    name = "Asa"
-    student_num = 911256
-    courses_num = 2
-    student = Student(name, student_num, courses_num)
-    student.print_values()
+    students = []
+    while True:
+        print("Enter name of student: ")
+        name = input()
+        print("Enter student id number: ")
+        student_num = input()
+        print("Enter number of enrolled courses: ")
+        courses_num = input()
+        student = Student(name, student_num, courses_num)
+        students.append(student)
+        cont = continue_adding()
+        if not cont:
+            break
+    for s in students:
+        s.print_values()
+
+
+
+
