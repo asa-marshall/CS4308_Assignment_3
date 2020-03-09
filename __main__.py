1c70687fa0c7dd02de4aab61662eed1d56562f70
asa <asam268@gmail.com>
2020-03-09 19:48:30 -0400

    Added Documentation.

--- __main__.py
+++ __main__.py
@@ -22,36 +22,52 @@ def continue_adding():
 
 
 if __name__ == '__main__':
+    # Define an array of students
     students = []
+
     while True:
+        # create a Student object
         student = Student()
+
+        # request name from input
         while True:
             try:
                 print("Enter name of student: ")
                 name = input()
+                # invoke a method to change the value of its attributes of the object
                 student.set_name(name)
                 break
             except ValueError:
                 print("Invalid name argument")
+
+        # request student number from input
         while True:
             try:
                 print("Enter student id number: ")
                 student_num = int(input())
+                # invoke a method to change the value of its attributes of the object
                 student.set_student_num(student_num)
                 break
             except ValueError:
                 print("Invalid student_num argument")
+
+        # request courses from input
         while True:
             try:
                 print("Enter number of enrolled courses: ")
                 courses = int(input())
+                # invoke a method to change the value of its attributes of the object
                 student.set_courses(courses)
                 break
             except ValueError:
                 print("Invalid courses argument")
+
+        # store the Student in the array
         students.append(student)
         if not continue_adding():
             break
+
+    # invoke a method that displays the value of each attribute of every student in the array
     print("\nPrinting student information...")
     for s in students:
         print(s)
